322eefd4104d560f6988c318bf89d6e7f7142b53
aparep <aliciaparedespezo@gmail.com>
2023-05-29 02:20:18 -0500

    Crear models en home

--- project/apps/home/models.py
+++ project/apps/home/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+class Tarea(models.Model):
+    descripcion = models.CharField(max_length=100)
+    duracionseg = models.IntegerField()
+
+    def __str__(self) -> str:
+        return self.descripcion
+
+class Responsable(models.Model):
+    nombre = models.CharField(max_length=100)
+    apellidos = models.CharField(max_length=100)
+    edad = models.IntegerField()
+
+    def __str__(self) -> str:
+        return self.nombre
+
+class Local(models.Model):
+    establecimiento = models.CharField(max_length=100)
+    distrito = models.CharField(max_length=100)
+
+    def __str__(self) -> str:
+        return self.establecimiento
